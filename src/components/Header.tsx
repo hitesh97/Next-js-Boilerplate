7c6583fc089564f3b535bb892b345582fe979f81
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2021-07-29 21:35:27 +0100

    responsive header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,88 +1,69 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
-
-import Link from 'next/link';
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [showMenu, setToggleMenu] = useState(false);
   return (
-    <header className="border-b-2 z-10 bg-gray-100 fixed inset-x-0 top-0">
-      <nav className="flex items-center justify-between p-6 container mx-auto">
-        <a href="#">logo</a>
-        <div className="text-lg text-gray-600 hidden lg:flex">
-          <Link href="/">
-            <a
-              href="#"
-              className="block mt-4 lg:inline-block text-teal-600 lg:mt-0 mr-10"
-            >
-              Home
-            </a>
-          </Link>
-          <Link href="/">
-            <a
-              href="#"
-              className="block mt-4 lg:inline-block hover:text-gray-700 lg:mt-0 mr-10"
-            >
-              Services
-            </a>
-          </Link>
-          <Link href="/">
-            <a
-              href="#"
-              className="block mt-4 lg:inline-block hover:text-gray-700 lg:mt-0 mr-10"
-            >
-              Portfolio
-            </a>
-          </Link>
-          <Link href="/about">
-            <a
-              href="#"
-              className="block hover:text-gray-700 mt-4 lg:inline-block lg:mt-0 mr-10"
-            >
-              Company
-            </a>
-          </Link>
-          <Link href="/">
-            <a
-              href="#"
-              className="block hover:text-gray-700 mt-4 lg:inline-block lg:mt-0"
-            >
-              Contact
-            </a>
-          </Link>
+    <header className="flex flex-wrap flex-row justify-between md:items-center md:space-x-4 py-4 px-6 border-b-2 z-10 bg-gray-100 fixed inset-x-0 top-0">
+      Logo here
+      <nav
+        className={`absolute md:relative top-16 left-0 md:top-0 z-20 md:flex flex-col md:flex-row md:space-x-6 font-semibold w-full md:w-auto shadow-md rounded-lg md:rounded-none md:shadow-none p-2 md:px-6 bg-gray-100 ${
+          showMenu ? 'flex' : 'hidden'
+        }`}
+      >
+        <div className="align-middle">
+          <a
+            href="#"
+            className="block py-1 text-indigo-600 hover:underline align-text-middle"
+          >
+            Home
+          </a>
         </div>
-
-        <div className="flex items-center">
-          <div className="mr-5 lg:mr-0">
-            <button
-              type="button"
-              className="py-2 px-6 rounded-md text-gray-600 hover:text-gray-700 text-lg"
-            >
-              Sign in
-            </button>
-            <button
-              type="button"
-              className="py-2 px-6 bg-blue-500 hover:bg-blue-600 rounded-md text-white text-lg"
-            >
-              Sign up
-            </button>
-          </div>
-          <div className="block lg:hidden">
-            <button
-              type="button"
-              className="flex items-center px-4 py-3 border rounded text-blue-500 border-blue-500 focus:outline-none"
-            >
-              <svg
-                className="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
-        </div>
+        <a href="#" className="block py-1 text-gray-600 hover:underline">
+          About us
+        </a>
+        <a href="#" className="block py-1 text-gray-600 hover:underline">
+          Services
+        </a>
+        <a href="#" className="block py-1 text-gray-600 hover:underline">
+          Blog
+        </a>
+        <a href="#" className="block py-1 text-gray-600 hover:underline">
+          Contact
+        </a>
       </nav>
+      <div className="flex items-center">
+        <div className="mr-5 lg:mr-0">
+          <button
+            type="button"
+            className="py-2 px-6 rounded-md text-gray-600 hover:text-gray-700 text-lg"
+          >
+            Sign in
+          </button>
+          <button
+            type="button"
+            className="py-2 px-6 bg-blue-500 hover:bg-blue-600 rounded-md text-white text-lg"
+          >
+            Sign up
+          </button>
+        </div>
+        <div className="block md:hidden lg:hidden">
+          <button
+            type="button"
+            className="flex items-center px-4 py-3 border rounded text-blue-500 border-blue-500 focus:outline-none"
+            onClick={() => setToggleMenu(!showMenu)}
+          >
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+      </div>
     </header>
   );
 };
